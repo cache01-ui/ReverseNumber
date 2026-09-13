1fb75537bf8f8c9a897218f1c4f7c44e32e99128
cache01-ui <delta.cache01@gmail.com>
2026-09-13 13:19:09 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Task1_ReverseNumber {
+    internal class Program {
+        static void Main(string[] args) {
+            while (true) {
+                Console.Write("Введіть ціле число: ");
+                string? input = Console.ReadLine();
+
+                if (string.IsNullOrWhiteSpace(input)) break;
+
+                int number = int.Parse(input);
+                bool isNegative = number < 0;
+                int tempNumber = Math.Abs(number);
+                int reversedNumber = 0;
+
+                while (tempNumber > 0) {
+                    int lastDigit = tempNumber % 10;
+                    reversedNumber = reversedNumber * 10 + lastDigit;
+                    tempNumber /= 10;
+                }
+
+                if (isNegative) {
+                    reversedNumber = -reversedNumber;
+                }
+
+                Console.WriteLine($"Результат: {reversedNumber}\n");
+            }
+        }
+    }
+}
+
